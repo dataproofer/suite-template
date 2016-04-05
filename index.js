36ae8a6024a93e68e2665d826fb0fbbb2ffe5e7f
geraldarthur <gerald.a.rich@gmail.com>
2016-04-04 18:05:48 -0700

    update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 exports = module.exports = {
   name: "test-suite-template", // a hyphenated, unique name
   fullName: "Test Suite Template", // a full name used for display in the desktop app
-  tests: [],      // the list of main tests to be run in the suite
-}
+  tests: [] // the list of main tests to be run in the suite
+};
 
-var myTest = require('./src/myTest');
-var myAdvancedTest = require('./src/myAdvancedTest');
+var myTest = require("./src/myTest");
+var myAdvancedTest = require("./src/myAdvancedTest");
 
 exports.tests.push(
   myTest,
